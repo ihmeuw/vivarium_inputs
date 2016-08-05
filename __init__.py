35efd2280406a32739990e0f109b7e421708fc50
Alec Deason <alecwd@uw.edu>
2016-08-05 12:08:10 -0700

    Proportion splits

--- __init__.py
+++ __init__.py
@@ -8,6 +8,9 @@ def get_excess_mortality(modelable_entity_id):
 def get_incidence(modelable_entity_id):
     return load_data_from_cache(get_modelable_entity_draws, 'rate', location_id=config.getint('simulation_parameters', 'location_id'), year_start=config.getint('simulation_parameters', 'year_start'), year_end=config.getint('simulation_parameters', 'year_end'), measure=6, me_id=modelable_entity_id)
 
+def get_proportion(modelable_entity_id):
+    return load_data_from_cache(get_modelable_entity_draws, 'proportion', location_id=config.getint('simulation_parameters', 'location_id'), year_start=config.getint('simulation_parameters', 'year_start'), year_end=config.getint('simulation_parameters', 'year_end'), measure=18, me_id=modelable_entity_id)
+
 def get_disease_states(population, states):
     location_id = config.getint('simulation_parameters', 'location_id')
     year_start = config.getint('simulation_parameters', 'year_start')
