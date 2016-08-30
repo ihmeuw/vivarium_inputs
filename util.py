61364a60f068bb7e4f993b287138122daed4f5b9
Alec Deason <alecwd@uw.edu>
2016-08-30 12:48:24 -0700

    Forgotten import

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import os.path
 import subprocess
 
+import pandas as pd
+
 from ceam import config
 
 STATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cen_functions_scripts')
