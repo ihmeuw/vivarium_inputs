042c7e7699057becabebadebc71b575a745babc9
Alec Deason <alecwd@uw.edu>
2016-10-17 10:21:27 -0700

    Fiddle structure to make it properly package like

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# ~/ceam/setup.py
+
+from setuptools import setup, find_packages
+
+
+setup(name='ceam_inputs',
+        version='0.1',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            'pandas',
+            'numpy',
+            'scipy',
+            'joblib',
+        ]
+     )
+
+
+# End.
