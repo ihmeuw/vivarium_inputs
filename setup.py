52251450e8653a6833f9b59059c62d13ddfba8b8
Alec Deason <alecwd@uw.edu>
2016-11-02 13:49:53 -0700

    try again with the dependencies (and again)

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(name='ceam_inputs',
             'numpy',
             'scipy',
             'joblib',
-            'db_tools==dev',
+            'db_tools',
         ],
         dependency_links=[
-            'ssh://git@stash.ihme.washington.edu:7999/cp/db_tools.git#egg=db_tools-dev',
+            'git+ssh://git@stash.ihme.washington.edu:7999/cp/db_tools.git#egg=db_tools-dev',
         ]
      )
 
