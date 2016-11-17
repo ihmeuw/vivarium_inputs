09655afb01f458107a94fab99a0351294ef88294
Alec Deason <alecwd@uw.edu>
2016-11-17 10:58:48 -0800

    cherry picking auxiliary files change

--- ceam_inputs/auxiliary_files.py
+++ ceam_inputs/auxiliary_files.py
@@ -23,10 +23,11 @@ FILES = {
         'path': '{j_drive}/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/GBD_2015/combined_dws.csv',
         'source': '/home/j/WORK/04_epi/03_outputs/01_code/02_dw/03_custom/combined_dws.csv',
         },
-    'Systolic Blood Pressure Distributions': {
-        'path': '{j_drive}/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/GBD_2015/systolic_blood_pressure/exp_{location_id}_{year_id}_{sex_id}.dta',
-        'source': '/share/epi/risk/paf/metab_sbp_interm',
-        'owner': 'Stan Biryukov <stan0625@uw.edu>',
+        'Systolic Blood Pressure Distributions': {
+            'path': '{j_drive}/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/GBD_2015/systolic_blood_pressure/exp_{location_id}_{year_id}_{sex_id}.dta',
+            'source': '/share/epi/risk/paf/metab_sbp_interm',
+            'encoding': 'latin1',
+            'owner': 'Stan Biryukov <stan0625@uw.edu>',
         },
     'Body Mass Index Distributions': {
         'path': '{j_drive}/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/GBD_2015/bmi/{parameter}/19_{location_id}_{year_id}_{sex_id}.csv',
@@ -36,13 +37,16 @@ FILES = {
 
 }
 
-def open_auxiliary_file(name, **kwargs):
+def auxiliary_file_path(name, **kwargs):
     template_parameters = dict(kwargs)
     if platform.system() == 'Windows':
         template_parameters['j_drive'] = 'J:'
     else:
         template_parameters['j_drive'] = '/home/j'
     raw_path = FILES[name]['path']
-    path = raw_path.format(**template_parameters)
+    return raw_path.format(**template_parameters)
+
+def open_auxiliary_file(name, **kwargs):
+    path = auxiliary_file_path(name, **kwargs)
     encoding = FILES[name].get('encoding')
     return open(path, encoding=encoding)
--- ceam_inputs/gbd_ms_functions.py
+++ ceam_inputs/gbd_ms_functions.py
@@ -20,7 +20,7 @@ from ceam import config
 from ceam.interpolation import Interpolation
 
 from ceam_inputs.util import stata_wrapper, get_cache_directory
-from ceam_inputs.auxiliary_files import open_auxiliary_file
+from ceam_inputs.auxiliary_files import open_auxiliary_file, auxiliary_file_path
 
 from ceam_inputs.gbd_ms_auxiliary_functions import set_age_year_index
 from ceam_inputs.gbd_ms_auxiliary_functions import interpolate_linearly_over_years_then_ages
@@ -933,11 +933,11 @@ def get_sbp_mean_sd(location_id, year_start, year_end):
     for sex_id in [1, 2]:
         draws = pd.DataFrame()
         for year_id in np.arange(year_start, year_end + 1, 5):
-            with open_auxiliary_file('Systolic Blood Pressure Distributions',
+            path = auxiliary_file_path('Systolic Blood Pressure Distributions',
                                      location_id=location_id,
                                      year_id=year_id,
-                                     sex_id=sex_id) as f:
-                one_year_file = pd.read_stata(f)
+                                     sex_id=sex_id)
+            one_year_file = pd.read_stata(path)
             one_year_file['year_id'] = year_id
             draws = draws.append(one_year_file)
 
