47998f904793a8026e66ab2fb01d7f11b87880c6
Alec Deason <alecwd@uw.edu>
2017-02-24 13:35:17 -0800

    Tweak things to use the internal pypi server for requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# ~/ceam/setup.py
-
 from setuptools import setup, find_packages
 
 
@@ -8,13 +5,15 @@ setup(name='ceam_inputs',
         version='0.1',
         packages=find_packages(),
         include_package_data=True,
+        package_index='http://dev-tomflem.ihme.washington.edu/simple/',
         install_requires=[
             'pandas',
             'numpy',
             'scipy',
             'joblib',
+            'flufl.lock',
+            'transmogrifier',
+            'hierarchies',
+            'db_tools',
         ]
      )
-
-
-# End.
