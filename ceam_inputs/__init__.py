6fa1ab38f65bed54bb843f7bcc53394683d8d698
James Collins <collijk@uw.edu>
2017-05-29 17:17:20 -0700

    Moved risk effect factory out of ceam-inputs and into ceam-public-health.util.risk

--- ceam_inputs/__init__.py
+++ ceam_inputs/__init__.py
@@ -303,15 +303,6 @@ def get_fpg_distributions():
     return distributions.get_fpg_distributions(location_id, year_start, year_end, draw)
 
 
-def make_gbd_risk_effects(risk_id, causes, effect_function, risk_name):
-    return [RiskEffect(
-        get_relative_risks(risk_id=risk_id, cause_id=cause_id),
-        get_pafs(risk_id=risk_id, cause_id=cause_id),
-        cause_name, risk_name,
-        effect_function)
-        for cause_id, cause_name in causes]
-
-
 def get_annual_live_births(location_id, year, sex_id=3):
     data = functions.load_data_from_cache(functions.get_covariate_estimates,
                                           covariate_name_short='live_births_by_sex',
