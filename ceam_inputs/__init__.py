b787712c458ac81a3a3b9fa9faec09ac48ee9d5b
emumford <emumford@uw.edu>
2017-06-23 11:07:01 -0700

    adding a select_draw_data call to get_dtp3_coverage

--- ceam_inputs/__init__.py
+++ ceam_inputs/__init__.py
@@ -565,4 +565,5 @@ def get_dtp3_coverage():
     location_id = config.simulation_parameters.location_id
     year_start, year_end = gbd_year_range()
     draw_number = config.run_configuration.draw_number
-    return functions.get_dtp3_coverage(location_id, year_start, year_end, draw_number)
+    draws = functions.get_dtp3_coverage(location_id, year_start, year_end, draw_number)
+    return functions.select_draw_data(draws, config.run_configuration.draw_number, column_name='coverage')
