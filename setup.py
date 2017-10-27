0107c4c98170f069f3ac9ea1404cb008abb499cd
James Collins <collijk@uw.edu>
2017-10-26 19:57:12 -0700

    Added missing dependencies, fixed pep8 stuff

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@ from setuptools import setup, find_packages
 
 
 setup(name='ceam_inputs',
-        version='0.1',
-        packages=find_packages(),
-        include_package_data=True,
-        install_requires=[
-            'pandas',
-            'numpy',
-            'scipy',
-            'joblib',
-        ],
-        extras_require = {
-            'gbd_access': [
-                'transmogrifier',
-                'hierarchies',
-                'db_tools',
-                ]
-        }
-     )
+      version='0.1',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'pandas',
+          'numpy',
+          'scipy',
+          'joblib',
+      ],
+      extras_require={
+          'gbd_access': [
+              'transmogrifier',
+              'hierarchies',
+              'db_tools',
+              'db_queries',
+              'risk_utils',
+          ]
+      }
+      )
