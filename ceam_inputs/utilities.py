1c82cf516de4cbebbea518d08f74bb2674a5c88c
Alec Georgoff <georgoff@cn497.ihme.washington.edu>
2017-11-17 17:05:22 -0800

    Added Alec and Paola's functions for age

--- ceam_inputs/utilities.py
+++ ceam_inputs/utilities.py
@@ -70,6 +70,51 @@ def standardize_data_for_age(data: pd.DataFrame, fill_na_value: float, extent: I
 
 ### END OF ALEC'S CODE ###
 
+### START OF PAOLA'S CODE ###
+
+def makeMissingData(data_frame, extent, fill_na_value):
+    data = data_frame.copy()
+    extent_missing_data = extent  # assume this is an input to the standardize_data_for_age function
+    age_ids_in_data = data['age_group_id'].unique()
+    missing_age_group_ids = extent_missing_data.difference(age_ids_in_data)
+    year_ids = set(data['year_id'])
+    location_ids = set(data['location_id'])
+    sex_ids = set(data['sex_id'])
+    age_group_ids = missing_age_group_ids
+    draw_columns = [f'draw_{i}' for i in range(0, 1000)]
+    value = fill_na_value
+    index = list(itertools.product(year_ids, sex_ids, age_group_ids,
+                                   location_ids))  # makes a list of tuples with all the possible combinations for the demographic columns
+    df = pd.DataFrame(
+        {'year_id': list(zip(*index))[0], 'sex_id': list(zip(*index))[1], 'age_group_id': list(zip(*index))[2],
+         'location_id': list(zip(*index))[3]})
+    df_dummy = pd.DataFrame(data=value, index=df.index, columns=draw_columns)
+    df1 = pd.concat([df, df_dummy], axis=1)
+    return df1
+
+
+def check_for_age_group_ids(data_frame, extent, value):
+    df_ids = set(data['age_group_id'])  # generates a set with age_group_ids in the df
+    extent = set(extent)  # turns list into set
+
+    # in this case data has too many age_group_ids
+    if not df_ids.issubset(extent):
+        # make a list of excess elements
+        extra_age_group_ids = df_ids.difference(extent)
+        # remove additional age_group_ids from data.
+        for x in extra_age_group_ids:
+            df_ids.discard(x)
+    if df_ids.issubset(extent):
+        clean_data = data[data['age_group_id'].isin(df_ids)]
+
+    if len(extent) > len(df_ids):
+        missing_data = makeMissingData(clean_data, extent, value)
+        data_with_correct_age_group_ids = pd.concat([missing_data, clean_data], axis=0)
+
+    return data_with_correct_age_group_ids
+
+### END OF PAOLA'S CODE ###
+
 
 def standardize_data_for_sex(data: pd.DataFrame, fill_na_value: float, extent: Iterable[int]) -> pd.DataFrame:
     # Potential inputs:
