51f3ddcc21e14512223b23f8cff583d17327d5af
James Collins <collijk@uw.edu>
2017-11-27 23:59:16 -0800

    Removed a bunch of unused references, updated some file paths

--- ceam_inputs/auxiliary_files.py
+++ ceam_inputs/auxiliary_files.py
@@ -8,17 +8,6 @@ from os.path import join
 AUXILIARY_DATA_FOLDER = "{j_drive}/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/{gbd_round}"
 
 FILES = {
-    'Angina Proportions' : {
-        'path': 'angina_prop_postMI.csv',
-        'source': '/snfs1/WORK/04_epi/01_database/02_data/cvd_ihd/04_models/02_misc_data/angina_prop_postMI.csv',
-        'owner': 'Catherine O. Johnson <johnsoco@uw.edu>',
-        },
-    'Age-Specific Fertility Rates': {
-        'path': 'ASFR.csv',
-        'encoding': 'latin1',
-        'source': 'covariate_short_name: Age-Specific Fertility Rate; model_version: 9065',
-        'owner': 'HAIDONG WANG <haidong@uw.edu>',
-        },
     'Disability Weights': {
         'path': 'dw.csv',
         'source': '/home/j/WORK/04_epi/03_outputs/01_code/02_dw/02_standard/dw.csv',
@@ -27,44 +16,26 @@ FILES = {
         'path': 'combined_dws.csv',
         'source': '/home/j/WORK/04_epi/03_outputs/01_code/02_dw/03_custom/combined_dws.csv',
         },
-    'Systolic Blood Pressure Distributions': {
-        # FIXME: This will go away when we get a consistent access pattern for exposure SDs.
-        # 'path': 'systolic_blood_pressure/exp_{location_id}_{sex_id}.dta',
-        'path': '/share/costeffectiveness/CEAM/Auxliary_Data/systolic_blood_pressure/exp_{location_id}_{sex_id}.dta',
-        'source': '/share/epi/risk/paf/metab_sbp/exposures',
-        'encoding': 'latin1',
-        'owner': 'Stan Biryukov <stan0625@uw.edu>',
-        },
-    'Body Mass Index Distributions': {
-        'path': 'bmi/{parameter}/19_{location_id}_{year_id}_{sex_id}.csv',
-        'source': '/share/covariates/ubcov/04_model/beta_parameters/8',
-        'owner': 'Marissa B. Reitsma <mreitsma@uw.edu>',
-        },
     'Ensemble Distribution Weights': {
         'path': 'ensemble/weights/{rei_id}.csv',
         'source': '/home/j/WORK/05_risk/ensemble/weights',
         'owner': 'Patrick J Sur <psur2417@uw.edu>, Stan Biryukov <stan0625@uw.edu>',
-        },
-    'Fasting Plasma Glucose Distributions': {
-        'path': 'fpg/FILE_{location_id}_{year_id}_{sex_id}_OUT.csv',
-        'source': '/share/epi/risk/paf/metab_fpg_cont_sll/FILE_[location_id]_[year_id]_[sex_id]_OUT.csv',
-        'owner': 'Stan Biryukov <stan0625@uw.edu>',
         },
     'Risk Factor Propensity Correlation Matrices': {
         'path': 'risk_factor_propensity_correlation_matricies/location_{location_id}',
         'owner': 'Reed Sorenson <rsoren@uw.edu>',
         },
     'Inpatient Visit Costs': {
-        'path': 'ip_cost.csv',
-        'owner': 'Mark Moses <mwm6@uw.edu>',
-    },
-    'Hypertension Drug Costs': {
-        'path': 'higashi_drug_costs_20160804.csv',
+        'path': 'costs/healthcare_entities/inpatient.hdf',
         'owner': 'Mark Moses <mwm6@uw.edu>',
     },
     'Outpatient Visit Costs': {
-        'path': 'op_cost.csv',
+        'path': 'costs/healthcare_entities/outpatient.hdf',
         'source': "/snfs1/Project/Cost_Effectiveness/Access_to_care/02_analysis/01_data/op_unit_cost.csv",
+        'owner': 'Mark Moses <mwm6@uw.edu>',
+    },
+    'Hypertension Drug Costs': {
+        'path': 'costs/treatment_technologies/hypertension_drugs.hdf',
         'owner': 'Mark Moses <mwm6@uw.edu>',
     },
     'Ors Exposure': {
@@ -98,16 +69,6 @@ FILES = {
         # FIXME: Everett to clean up source code after distribution is chosen and put code in CEAM
         'source': '',
         'owner': 'Everett Mumford <emumford@uw.edu>',
-    },
-    'Diarrhea Costs': {
-        'path':  'op_ip_diarrhea_cost_all_country_years.csv',
-        'source':  '/home/j/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/GBD_2015/op_ip_diarrhea_cost_all_country_years.csv',
-        'owner': 'Mark Moses <mwm6@uw.edu>',
-    },
-    'ORS Costs': {
-        'path': 'healthcare_access/ors_cost/{location_id}.csv',
-        'source':  '/home/j/Project/Cost_Effectiveness/CEAM/Auxiliary_Data/GBD_2015/op_ip_diarrhea_cost_all_country_years.csv',
-        'owner': 'Mark Moses <mwm6@uw.edu>',
     },
     'Risk Data': {
         'path': 'risk_variables.xlsx',
