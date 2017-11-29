d6ee580795d83a17c92548c97afcb9f929d3a02a
James Collins <collijk@uw.edu>
2017-11-28 22:16:03 -0800

    Updating mapping to reflect usage and changes in auxiliary data

--- ceam_inputs/gbd_mapping/treatment_technologies.py
+++ ceam_inputs/gbd_mapping/treatment_technologies.py
@@ -6,7 +6,7 @@ from .templates import ModelableEntity, GbdRecord, meid
 class TreatmentTechnology(ModelableEntity):
     """Container for treatment technology GBD ids and data."""
     __slots__ = ('name', 'unit_cost', 'cost', 'coverage', 'protection',
-                 'efficacy_mean', 'efficacy_sd', 'pafs', 'rrs', 'exposures', )
+                 'efficacy_mean', 'efficacy_sd', 'population_attributable_fraction', 'relative_risk', 'exposures', )
 
     def __init__(self,
                  name: str,
@@ -17,8 +17,8 @@ class TreatmentTechnology(ModelableEntity):
                  protection: str = None,
                  efficacy_mean: float = None,
                  efficacy_sd: float = None,
-                 pafs: str = None,
-                 rrs: str = None,
+                 population_attributable_fraction: str = None,
+                 relative_risk: str = None,
                  exposures: str = None,):
         super().__init__(name=name, gbd_id=gbd_id)
         self.unit_cost = unit_cost
@@ -27,23 +27,21 @@ class TreatmentTechnology(ModelableEntity):
         self.protection = protection
         self.efficacy_mean = efficacy_mean
         self.efficacy_sd = efficacy_sd
-        self.pafs = pafs
-        self.rrs = rrs
+        self.population_attributable_fraction = population_attributable_fraction
+        self.relative_risk = relative_risk
         self.exposures = exposures
 
 
 class TreatmentTechnologies(GbdRecord):
     """Holder for treatment technology records."""
-    __slots__ = ('ors', 'rota_vaccine', 'dtp3_vaccine', 'hypertension_drugs', )
+    __slots__ = ('ors', 'rota_vaccine', 'hypertension_drugs', )
 
     def __init__(self,
                  ors: TreatmentTechnology,
                  rota_vaccine: TreatmentTechnology,
-                 dtp3_vaccine: TreatmentTechnology,
                  hypertension_drugs: TreatmentTechnology, ):
         self.ors = ors
         self.rota_vaccine = rota_vaccine
-        self.dtp3_vaccine = dtp3_vaccine
         self.hypertension_drugs = hypertension_drugs
 
 
@@ -54,21 +52,14 @@ treatment_technologies = TreatmentTechnologies(
         cost='ORS Costs',
         unit_cost=0.50,  # As per Marcia 07/01/2017
         coverage=0.58,  # As per Marcia 07/01/2017
-        pafs='Ors Pafs',
-        rrs='Ors Relative Risks',
-        exposures='Ors Exposure',
+        population_attributable_fraction='Ors Pafs',
+        relative_risk='Ors Relative Risks',
     ),
     rota_vaccine=TreatmentTechnology(
         name='rota_vaccine',
         gbd_id=None,
         coverage=meid(10596),
         protection='Rota Vaccine Protection',
-        rrs='Rota Vaccine RRs',
-    ),
-    dtp3_vaccine=TreatmentTechnology(
-        name='dtp3_vaccine',
-        gbd_id=None,
-        unit_cost=0,
     ),
     hypertension_drugs=TreatmentTechnology(
         name='hypertension_drugs',
