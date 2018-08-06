07d88cdf928b151b12e2771f823390fddcc1181f
Cody <chorst@uw.edu>
2018-08-06 13:27:05 -0700

    add location option and sensible defaults

--- scripts/build_artifact.py
+++ scripts/build_artifact.py
@@ -1,4 +1,6 @@
 import logging
+import getpass
+import os
 import argparse
 
 from vivarium.framework.configuration import build_model_specification
@@ -6,15 +8,17 @@ from vivarium.framework.plugins import PluginManager
 from vivarium.interface.interactive import InteractiveContext
 
 
-def main():
+def _build_artifact():
     parser = argparse.ArgumentParser()
     parser.add_argument('simulation_configuration', type=str)
-    parser.add_argument('--output_path', type=str)
+    parser.add_argument('--output_root', type=str, optional=True)
+    parser.add_argument('--location', type=str, optional=True)
     parser.add_argument('--from_scratch', '-s', action="store_true",
                         help="Do not reuse any data in the artifact, if any exists")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
+
     model_specification = build_model_specification(args.simulation_configuration)
     model_specification.plugins.optional.update({
         "data": {
@@ -22,11 +26,14 @@ def main():
             "builder_interface": "vivarium_public_health.dataset_manager.ArtifactManagerInterface",
         }})
 
-    output_path = get_output_path(args.output_path, model_specification.configuration)
+    update_configuration(args.simulation_configuration, args.location, args.output_root,
+                         model_specification.configuration)
 
     plugin_config = model_specification.plugins
     component_config = model_specification.components
     simulation_config = model_specification.configuration
+
+    output_path = simulation_config.artifact.path
 
     plugin_manager = PluginManager(plugin_config)
     component_config_parser = plugin_manager.get_plugin('component_configuration_parser')
@@ -40,15 +47,37 @@ def main():
     simulation.data.end_processing()
 
 
-def get_output_path(command_line_arg, configuration):
-    if command_line_arg:
-        return command_line_arg
-    elif 'artifact' in configuration and 'path' in configuration.artifact and configuration.artifact.path is not None:
-        return configuration.artifact.path
+def update_configuration(specification_arg, location_arg, output_root_arg, configuration):
+    """Update the simulation configuration artifact output path and location with 
+    command line inputs."""
+
+    specification_file = os.path.basename(specification_arg)
+    specification_name = os.path.splitext(specification_file)[0]
+
+    if not location_arg and not output_root_arg:
+        if ('input_data' in configuration and 'location' in configuration.input_data and 
+            configuration.input_data.location):
+            configuration.artifact.path = os.path.join('\\', 'share', 'scratch', getpass.getuser(),
+                'vivarium_artifacts', specification_name + '.hdf')
+        else
+            raise argparse.ArgumentError(
+                "specify a location or include configuration.input_data.location in model specification")
+    elif not location_arg and output_root_arg 
+        if ('input_data' in configuration and 'location' in configuration.input_data and 
+            configuration.input_data.location):
+            configuration.artifact.path = os.path.join(output_root_arg, 'specification_name' + '.hdf')
+        else:
+            raise argparse.ArgumentError(
+                "specify a location or include configuration.input_data.location in model specification")
+    elif location_arg and not output_root_arg
+        configuration.input_data.location = location_arg
+        configuration.artifact.path = os.path.join('\\', 'share', 'scratch', getpass.getuser(),
+            'vivarium_artifacts', specification_name + f'_{location_arg}.hdf')
     else:
-        raise argparse.ArgumentError(
-            "specify --output_path or include configuration.artifact.path in model specification")
+        configuration.input_data.location = location_arg
+        configuration.artifact.path = os.path.join(output_root_arg,
+                                                   'specification_name' + f'_{location_arg}.hdf')
 
 
 if __name__ == "__main__":
-    main()
+    _build_artifact()
