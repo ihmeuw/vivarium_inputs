5a3d1ad2e90264777399c1802bc3f02095c58676
Michelle Park <hpark3@uw.edu>
2018-12-20 09:28:37 -0800

    add metadata

--- src/vivarium_inputs/data_artifact/builder.py
+++ src/vivarium_inputs/data_artifact/builder.py
@@ -27,6 +27,9 @@ class ArtifactBuilder:
         draw = builder.configuration.input_data.input_draw_number
 
         self.location = builder.configuration.input_data.location
+        hdf.write(path, EntityKey('metadata.location'), [self.location])
+        hdf.write(path, EntityKey('metadata.keyspace'), ['metadata.location', 'metadata.keyspace'])
+
         self.artifact = Artifact(path, filter_terms=[f'draw == {draw}', get_location_term(self.location)])
         self.modeled_causes = {c.cause for c in builder.components.get_components(DiseaseModel)}
         self.processed_entities = set()
