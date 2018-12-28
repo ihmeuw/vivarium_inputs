8f82ffbe568f4664537bee307bc4c5553ce3c0fd
Michelle Park <hpark3@uw.edu>
2018-12-28 12:11:46 -0800

    add some tests

--- src/vivarium_inputs/data_artifact/builder.py
+++ src/vivarium_inputs/data_artifact/builder.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 import warnings
+from tables.exceptions import NoSuchNodeError
 from typing import Collection, Any
 import pandas as pd
 from pathlib import Path
@@ -15,6 +16,10 @@ from vivarium_inputs.data_artifact.utilities import get_versions
 
 
 _log = logging.getLogger(__name__)
+
+
+class OutdatedArtifactWarning(Warning):
+    pass
 
 
 class ArtifactBuilder:
@@ -40,39 +45,42 @@ class ArtifactBuilder:
         builder.event.register_listener('post_setup', self.end_processing)
 
     @staticmethod
-    def initialize_artifact(path, append, draw, location):
-        def create_new_artifact():
-            art = Artifact(path, filter_terms=[f'draw == {draw}', get_location_term(location)])
-            art.write('metadata.versions', get_versions())
-            art.write('metadata.locations', [location])
-            return art
+    def initialize_artifact(path: str, append: bool, draw: int, location: str) -> Artifact:
+        """
+        For the given arguments, it checks all the basic conditions and
+        initialize the artifact. For now, all the outdated artifacts which
+        do not have proper metadata or not consistent metadata will not be
+        appended. we will wipe it out and build a new artifact.
+        """
 
-        if not append:
-            if Path(path).is_file():
-                Path(path).unlink()
-            artifact = create_new_artifact()
+        if append:
+            if not Path(path).is_file():
+                raise ValueError(f'{path} is not a file. You should provide the existing artifact path to append')
+
+            try:
+                artifact = Artifact(path, filter_terms=[f'draw == {draw}', get_location_term(location)])
+                if EntityKey('metadata.locations') not in artifact:
+                    warnings.warn('We will build from scratch', OutdatedArtifactWarning)
+                    artifact = create_new_artifact(path, draw, location)
+
+                elif artifact.load('metadata.locations') != [location]:
+                    raise ValueError(f"Artifact has {artifact.load('metadata.locations')} and we cannot append {location}")
+
+                if EntityKey('metadata.versions') not in artifact:
+                    warnings.warn('We will build from scratch', OutdatedArtifactWarning)
+                    artifact = create_new_artifact(path, draw, location)
+
+                elif artifact.load('metadata.versions') != get_versions():
+                    warnings.warn('Your artifact was made under different versions. We will wipe it out', DeprecationWarning)
+                    artifact = create_new_artifact(path, draw, location)
+
+            except NoSuchNodeError:
+                #  it means that path was a file but does not have metadata.keyspace inside
+                warnings.warn('We will wipe it out and build from scratch', OutdatedArtifactWarning)
+                artifact = create_new_artifact(path, draw, location)
 
         else:
-            artifact = Artifact(path, filter_terms=[f'draw == {draw}', get_location_term(location)])
-            if EntityKey('metadata.keyspace') not in artifact:
-                _log.debug('This is an outdated artifact. We will build from scratch')
-                Path(path).unlink()
-                artifact = create_new_artifact()
-
-            if EntityKey('metadata.locations') not in artifact:
-                _log.debug('This is an outdated artifact. We will build from scratch')
-                artifact = create_new_artifact()
-
-            elif artifact.load('metadata.locations')!= [location]:
-                raise ValueError(f"Artifact has {artifact.load('metadata.locations')} and we cannot append {location}")
-
-            if EntityKey('metadata.versions') not in artifact:
-                _log.debug('This is an outdated artifact. We will build from scratch')
-                artifact = create_new_artifact()
-
-            elif artifact.load('metadata.versions') != get_versions():
-                warnings.warn('Your artifact was made under different versions. We will wipe it out', DeprecationWarning)
-                artifact = create_new_artifact()
+            artifact = create_new_artifact(path, draw, location)
 
         return artifact
 
@@ -99,3 +107,12 @@ def _worker(entity_key: EntityKey, location: str, modeled_causes: Collection[str
     else:
         data = loader(entity_key, location, modeled_causes, all_measures=False)
     artifact.write(entity_key, data)
+
+
+def create_new_artifact(path: str, draw: int, location: str) -> Artifact:
+    if Path(path).is_file():
+        Path(path).unlink()
+    art = Artifact(path, filter_terms=[f'draw == {draw}', get_location_term(location)])
+    art.write('metadata.versions', get_versions())
+    art.write('metadata.locations', [location])
+    return art
