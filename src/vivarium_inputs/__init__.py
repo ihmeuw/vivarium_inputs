da88cbc5fd9c50e255f6aad7b328d2ab3d67c6c5
Michelle Park <hpark3@uw.edu>
2018-12-31 16:05:21 -0800

    roll back the change in init

--- src/vivarium_inputs/__init__.py
+++ src/vivarium_inputs/__init__.py
@@ -1,9 +1,1 @@
 from vivarium_inputs.interface import *
-
-from vivarium_inputs.__about__ import (__author__, __copyright__, __email__, __license__, __summary__, __title__,
-                                       __uri__, __version__, )
-
-__all__ = [__author__, __copyright__, __email__,
-           __license__, __summary__, __title__, __uri__, __version__, ]
-
-
