de485285654413b7848d3791a5e2cfd395ab5ea3
Michelle Park <hpark3@uw.edu>
2019-01-03 17:56:47 -0800

    missing set

--- src/vivarium_inputs/extract.py
+++ src/vivarium_inputs/extract.py
@@ -72,7 +72,7 @@ def get_sequela_disability_weight(entity: Sequela, _) -> pd.DataFrame:
 def check_years(df: pd.DataFrame, year_type: str):
     years = {'annual': [y for y in range(1990, 2018)], 'binned': gbd.get_estimation_years()}
     expected_years = years[year_type]
-    if set(df.year_id.unique()) < expected_years:
+    if set(df.year_id.unique()) < set(expected_years):
         raise DataNotExistError(f'Data has missing years: {set(expected_years).difference(set(df.year_id.unique()))}')
 
 
