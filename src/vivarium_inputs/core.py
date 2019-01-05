bcdc9055532a7a57acf1eee9b1954552b3a14fa9
James Collins <collijk@uw.edu>
2019-01-05 00:09:28 -0800

    add methods for scrubbing gbd conventions

---
+++ src/vivarium_inputs/core.py
@@ -0,0 +1,3 @@
+
+def get_data(entity, measure, location):
+    raise NotImplementedError()
--- src/vivarium_inputs/interface.py
+++ src/vivarium_inputs/interface.py
@@ -3,8 +3,7 @@ from vivarium_inputs import core, validation, utilities
 
 
 def get_measure(entity, measure, location):
-    core_func = getattr(core, f'get_{measure}')
-    data = core_func(entity, location)
-    data = utilities.scrub_gbd_conventions(data)
+    data = core.get_data(entity, measure, location)
+    data = utilities.scrub_gbd_conventions(data, location)
     validation.validate_for_simulation(data, entity, measure, location)
     return data
