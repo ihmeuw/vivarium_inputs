b350e4196a0e7bda507c5decb202c1c0bb763de5
Michelle Park <hpark3@uw.edu>
2019-01-11 13:07:58 -0800

    add alternative risk factors

--- src/vivarium_inputs/mapping_extension.py
+++ src/vivarium_inputs/mapping_extension.py
@@ -1,8 +1,9 @@
-from typing import Union
+from typing import Union, Tuple
 
-from gbd_mapping.id import meid
+from gbd_mapping.id import meid, reiid
 from gbd_mapping.base_template import GbdRecord, ModelableEntity
-
+from gbd_mapping.cause_template import Cause
+from gbd_mapping.cause import causes
 
 class HealthcareEntity(ModelableEntity):
     """Container for healthcare system GBD ids and data."""
@@ -45,6 +46,33 @@ class HealthTechnologies(GbdRecord):
         self.hypertension_drugs = hypertension_drugs
 
 
+class AlternativeRiskFactor(ModelableEntity):
+    """Container for risk factor outside of GBD and its metadata."""
+    __slots__ = ('name', 'kind', 'gbd_id', 'distribution', 'affected_causes')
+    def __init__(self,
+                 name: str,
+                 kind: str,
+                 gbd_id: Union[reiid, None],
+                 distribution: str,
+                 affected_causes: Tuple[Cause, ...]):
+        super().__init__(name=name, kind=kind, gbd_id=gbd_id)
+        self.distribution=distribution
+        self.affected_causes=affected_causes
+
+
+class AlternativeRiskFactors(GbdRecord):
+    """Holder of alternative risk factors."""
+    __slots__ =('child_wasting', 'child_underweight', 'child_stunting', )
+
+    def __init__(self,
+                 child_stunting: AlternativeRiskFactor,
+                 child_underweight: AlternativeRiskFactor,
+                 child_wasting: AlternativeRiskFactor,):
+        self.child_stunting = child_stunting
+        self.child_underweight = child_underweight
+        self.child_wasting = child_wasting
+
+
 healthcare_entities = HealthcareEntities(
     outpatient_visits=HealthcareEntity(
         name='outpatient_visits',
@@ -65,5 +93,41 @@ health_technologies = HealthTechnologies(
         name='hypertension_drugs',
         kind='health_technology',
         gbd_id=None,
+    )
+)
+
+
+alternative_risk_factors = AlternativeRiskFactors(
+    child_stunting=AlternativeRiskFactor(
+        name='child_stunting',
+        kind='alternative_risk_factor',
+        gbd_id=241,
+        distribution='ensemble',
+        affected_causes=(causes.all_causes, causes.communicable_maternal_neonatal_and_nutritional_diseases,
+                         causes.diarrheal_diseases, causes.lower_respiratory_infections, causes.measles,
+                         causes.respiratory_infections_and_tuberculosis, causes.enteric_infections,
+                         causes.other_infectious_diseases, ),
+    ),
+    child_wasting=AlternativeRiskFactor(
+        name='child_wasting',
+        kind='alternative_risk_factor',
+        gbd_id=240,
+        distribution='ensemble',
+        affected_causes=(causes.all_causes, causes.communicable_maternal_neonatal_and_nutritional_diseases,
+                         causes.diarrheal_diseases, causes.lower_respiratory_infections, causes.measles,
+                         causes.nutritional_deficiencies, causes.protein_energy_malnutrition,
+                         causes.respiratory_infections_and_tuberculosis, causes.enteric_infections,
+                         causes.other_infectious_diseases, ),
+    ),
+    child_underweight=AlternativeRiskFactor(
+        name='child_underweight',
+        kind='alternative_risk_factor',
+        gbd_id=94,
+        distribution='ensemble',
+        affected_causes=(causes.all_causes, causes.communicable_maternal_neonatal_and_nutritional_diseases,
+                         causes.diarrheal_diseases, causes.lower_respiratory_infections, causes.measles,
+                         causes.nutritional_deficiencies, causes.protein_energy_malnutrition,
+                         causes.respiratory_infections_and_tuberculosis, causes.enteric_infections,
+                         causes.other_infectious_diseases, ),
     )
 )
