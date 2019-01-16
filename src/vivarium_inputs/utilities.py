3dff48e0879a521dbd27fbcd05370fe210a90d2f
Kate Wilson <kwilson7@uw.edu>
2019-01-16 08:58:25 -0800

    cleanup

--- src/vivarium_inputs/utilities.py
+++ src/vivarium_inputs/utilities.py
@@ -133,6 +133,7 @@ def normalize_year(data: pd.DataFrame) -> pd.DataFrame:
         data = interpolate_year(data)
     else:  # set(data.year_id.unique()) == years['annual']
         pass
+
     # Dump extra data.
     data = data[data.year_id.isin(years['annual'])]
     return data
