58f8c68c5968951e1bda5aa8f5d9cd7b547ff0be
Cody <chorst@uw.edu>
2019-01-25 14:43:39 -0800

    bugfix norm_sex

--- src/vivarium_inputs/utilities.py
+++ src/vivarium_inputs/utilities.py
@@ -118,7 +118,7 @@ def normalize_sex(data: pd.DataFrame, fill_value) -> pd.DataFrame:
         data.loc[:, 'sex_id'] = 1
         fill_data.loc[:, 'sex_id'] = 2
         data = pd.concat([data, fill_data], ignore_index=True)
-    elif sexes == 1:
+    elif len(sexes) == 1:
         # Data is sex specific, but only applies to one sex, so fill the other with default.
         fill_data = data.copy()
         missing_sex = {1, 2}.difference(set(data.sex_id.unique())).pop()
