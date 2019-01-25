8fbcb6ec08e9c0dd51e1ebb9214ffe1d0d7fb0be
Cody <chorst@uw.edu>
2019-01-25 15:27:38 -0800

    finish normalize_sex test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -5,8 +5,6 @@ import pandas as pd
 from vivarium_inputs import utilities
 
 
-# assert no sex 3
-# assert sex 1 and 2 present
 @pytest.mark.parametrize("sex_ids", [
     (1, 1, 1, 2, 2, 2),
     (1, 1, 2, 2, 3, 3),
@@ -16,20 +14,33 @@ from vivarium_inputs import utilities
 ], ids=['male_female', 'male_female_both', 'male', 'female', 'both'])
 def test_normalize_sex(sex_ids):
     df = pd.DataFrame({'sex_id': sex_ids, 'value': [1] * len(sex_ids)})
-
     normalized = utilities.normalize_sex(df, fill_value=0.0)
-
     assert {1, 2} == set(normalized.sex_id)
 
 
-def test_normalize sex_fill(sex_ids):
-    pass
+def test_normalize_sex_copy_3():
+    values = [1, 2, 3, 4]
+    df = pd.DataFrame({'sex_id': [3] * len(values), 'value': values})
+    normalized = utilities.normalize_sex(df, fill_value=0.0)
+    assert (normalized.loc[normalized.sex_id == 1, 'value'] == values).all()
+    assert (normalized.loc[normalized.sex_id == 2, 'value'] == values).all()
 
 
+def test_normalize_sex_fill_value():
+    values = [1, 2, 3, 4]
+    fill = 0.0
+    for sex in [1, 2]:
+        missing_sex = 1 if sex == 2 else 2
+        df = pd.DataFrame({'sex_id': [sex] * len(values), 'value': values})
+        normalized = utilities.normalize_sex(df, fill_value=fill)
+        assert (normalized.loc[normalized.sex_id == sex, 'value'] == values).all()
+        assert (normalized.loc[normalized.sex_id == missing_sex, 'value'] == [fill] * len(values)).all()
 
-# assert dataframe is unchanged
+
 def test_normalize_sex_no_sex_id():
-    pass
+    df = pd.DataFrame({"ColumnA": [1, 2, 3], "ColumnB": [1, 2, 3]})
+    normalized = utilities.normalize_sex(df, fill_value=0.0)
+    pd.testing.assert_frame_equal(df, normalized)
 
 
 # # get bins, add and subtract years from
