70d661259741cbfb03a045dd2168c02921686974
Michelle Park <hpark3@uw.edu>
2019-02-01 16:13:27 -0800

    do not remove the raw validation for other meausre

--- src/vivarium_inputs/validation/raw.py
+++ src/vivarium_inputs/validation/raw.py
@@ -357,12 +357,15 @@ def _validate_exposure(data: pd.DataFrame, entity: Union[RiskFactor, CoverageGap
 
     if entity.kind == 'risk_factor':
         restrictions = entity.restrictions
+        age_start = get_restriction_age_boundary(entity, 'start')
+        age_end = get_restriction_age_boundary(entity, 'end')
         male_expected = restrictions.male_only or (not restrictions.male_only and not restrictions.female_only)
         female_expected = restrictions.female_only or (not restrictions.male_only and not restrictions.female_only)
 
-        cats.apply(check_age_group_ids, None, None)
+        cats.apply(check_age_group_ids, age_start, age_end)
         cats.apply(check_sex_ids, male_expected, female_expected)
 
+        cats.apply(check_age_restrictions, age_start, age_end)
         cats.apply(check_sex_restrictions, entity.restrictions.male_only, entity.restrictions.female_only)
 
         # we only have metadata about tmred for risk factors
@@ -404,9 +407,13 @@ def _validate_exposure_standard_deviation(data: pd.DataFrame, entity: Union[Risk
     check_location(data, location_id)
 
     if entity.kind == 'risk_factor':
-        check_age_group_ids(data, None, None)
+        age_start = get_restriction_age_boundary(entity, 'start')
+        age_end = get_restriction_age_boundary(entity, 'end')
+
+        check_age_group_ids(data, age_start, age_end)
         check_sex_ids(data, True, True)
 
+        check_age_restrictions(data, age_start, age_end)
         check_sex_restrictions(data, entity.restrictions.male_only, entity.restrictions.female_only)
     else:
         check_age_group_ids(data, None, None)
