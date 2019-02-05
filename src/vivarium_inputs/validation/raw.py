447204abe67c5b32f6c2c4c967063fd85ff7888d
Cody <chorst@uw.edu>
2019-02-05 13:55:53 -0800

    need measure id, not string

--- src/vivarium_inputs/validation/raw.py
+++ src/vivarium_inputs/validation/raw.py
@@ -7,7 +7,7 @@ import numpy as np
 from gbd_mapping import (ModelableEntity, Cause, Sequela, RiskFactor,
                          Etiology, Covariate, CoverageGap, causes)
 
-from vivarium_inputs.globals import (DRAW_COLUMNS, DEMOGRAPHIC_COLUMNS, SEXES, SPECIAL_AGES,
+from vivarium_inputs.globals import (DRAW_COLUMNS, DEMOGRAPHIC_COLUMNS, SEXES, SPECIAL_AGES, MEASURES,
                                      DataAbnormalError, InvalidQueryError, gbd, Population)
 from vivarium_inputs.mapping_extension import AlternativeRiskFactor, HealthcareEntity, HealthTechnology
 from vivarium_inputs.validation.utilities import (check_years, check_location, check_columns, check_data_exist,
@@ -965,13 +965,12 @@ def validate_population_attributable_fraction(data: pd.DataFrame, entity: Union[
 
     for c_id in data.cause_id:
         cause = [c for c in causes if c.gbd_id == c_id][0]
-        if cause.restrictions.yld_only and (data.measure_id == 'YLLs').any():
+        if cause.restrictions.yld_only and (data.measure_id == MEASURES['YLLs']).any():
             raise DataAbnormalError(f'Paf data for {entity.kind} {entity.name} affecting {cause.name} contains yll '
                                     f'values despite the affected entity being restricted to yld only.')
-        if cause.restrictions.yll_only and (data.measure_id == 'YLDs').any():
+        if cause.restrictions.yll_only and (data.measure_id == MEASURES['YLDs']).any():
             raise DataAbnormalError(f'Paf data for {entity.kind} {entity.name} affecting {cause.name} contains yld '
                                     f'values despite the affected entity being restricted to yll only.')
-
 
 def validate_mediation_factors(data, entity, location_id) -> None:
     raise NotImplementedError()
