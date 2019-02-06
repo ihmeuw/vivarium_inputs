1161790fc4f99071b0808b8b97333ad213753661
Michelle Park <hpark3@uw.edu>
2019-02-06 12:17:05 -0800

    use equality check

--- src/vivarium_inputs/core.py
+++ src/vivarium_inputs/core.py
@@ -167,8 +167,8 @@ def get_exposure(entity: Union[RiskFactor, AlternativeRiskFactor, CoverageGap], 
 
     if entity.distribution in ['dichotomous', 'ordered_polytomous', 'unordered_polytomous']:
         tmrel_cat = sorted(list(entity.categories.to_dict()), key=lambda x: int(x[3:]))[-1]
-        exposed = data[~data.parameter.isin([tmrel_cat])]
-        unexposed = data.loc[data.index.difference(exposed.index)]
+        exposed = data[data.parameter != tmrel_cat]
+        unexposed = data[data.parameter == tmrel_cat]
 
         #  FIXME: We fill 1 as exposure of tmrel category, which is not correct.
         data = pd.concat([utilities.normalize(exposed, fill_value=0), utilities.normalize(unexposed, fill_value=1)],
