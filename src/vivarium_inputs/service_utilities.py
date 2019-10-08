fcf23338084707f5958723f7516af14608e50f45
kjells <kjells@uw.edu>
2019-10-08 13:39:16 -0700

    Initial service commit targeted to 3.1 branch

---
+++ src/vivarium_inputs/service_utilities.py
@@ -0,0 +1,49 @@
+import gzip
+import io
+from pathlib import Path
+import pandas as pd
+import requests as req
+from loguru import logger
+
+
+TIMEOUT_SERVICE = None
+
+SERVICE_VERSION = '1'
+BASE_URL = f'http://microsim-rancher-p01.hosts.ihme.washington.edu:5000/v{SERVICE_VERSION}'
+#ARTIFACT_FOLDER = Path('/share/costeffectiveness/artifacts/')
+
+
+class GbdServiceError(Exception):
+    """Error for failures accessing GBD data."""
+    pass
+
+
+def dataframe_from_response(resp: req.Response) -> pd.DataFrame:
+    if resp.ok:
+        # return pickle.loads(gzip.decompress(resp.content))
+        # return resp.content.decode("utf-8")
+        return pd.read_csv(io.BytesIO(gzip.decompress(resp.content)), index_col=0)
+        # return pd.read_json(resp.content)
+    else:
+        logger.error(f'GbdServiceError: http response code {resp}')
+        raise GbdServiceError
+
+
+def build_url(endpoint_category: str, endpoint: str, params: str = None) -> str:
+    url = f'{BASE_URL}/{endpoint_category}/{endpoint}'
+    if params:
+        url = f'{url}?{params}'
+    return url
+
+
+def make_request(url: str) -> req.Response:
+    logger.info(f'Request start: {url}')
+    resp = req.get(url, timeout=None)
+    logger.info(f'Request end: {resp}')
+    return resp
+
+
+def check_response(resp: req.Response) -> None:
+    if not resp.ok:
+        logger.error(f'GbdServiceError: http response code {resp}')
+        raise GbdServiceError
