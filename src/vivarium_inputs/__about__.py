e038b78eee65bac9a7ff2b98b2cb0b22425dc1a8
James Collins <collijk@uw.edu>
2022-02-12 04:23:28 -0800

    Update metadata for release

--- src/vivarium_inputs/__about__.py
+++ src/vivarium_inputs/__about__.py
@@ -7,10 +7,10 @@ __title__ = "vivarium_inputs"
 __summary__ = "Transformations and artifact building for the vivarium microsimulation project."
 __uri__ = "https://github.com/ihmeuw/vivarium_inputs"
 
-__version__ = "4.0.4"
+__version__ = "4.0.5"
 
 __author__ = "The vivarium developers"
 __email__ = "vivarium.dev@gmail.com"
 
-__license__ = "GNU GPLv3"
-__copyright__ = f"Copyright 2016-2019 {__author__}"
+__license__ = "BSD-3-Clause"
+__copyright__ = f"Copyright 2022 {__author__}"
