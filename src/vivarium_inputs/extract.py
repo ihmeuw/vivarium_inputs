f8d04c95b6da8db238f9215199a7592d323b1332
hussain-jafari <hussainsjafari@gmail.com>
2024-02-21 13:11:45 -0600

    update extract_data docstring

--- src/vivarium_inputs/extract.py
+++ src/vivarium_inputs/extract.py
@@ -44,6 +44,9 @@ def extract_data(
         should be extracted and whether raw validation should be performed.
         Should only be set to False if data is being extracted for
         investigation. Never extract data for a simulation without validation.
+    get_all_years
+        Flag indicating whether to get all years. Otherwise, get most recent year.
+        Defaults to False.
 
     Returns
     -------
