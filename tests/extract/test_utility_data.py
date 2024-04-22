3805293d8dc38a0e7194d7435caacb60c13c4a72
albrja <albrja@uw.edu>
2024-04-22 14:30:41 -0700

    Update test

--- tests/extract/test_utility_data.py
+++ tests/extract/test_utility_data.py
@@ -32,8 +32,13 @@ def test_get_location_id():
     assert 163 == result_india
 
 
-def test_get_location_id_parents():
-    assert [1, 158, 159, 163] == ud.get_location_id_parents(163)
+@pytest.mark.parametrize(
+    "location_id, expected",
+    (163, {163: [1, 158, 159, 163]}),
+    ([163, 175], {163: [1, 158, 159, 163], 175: [1, 158, 159, 175]}),
+)
+def test_get_location_id_parents(location_id, expected):
+    assert expected == ud.get_location_id_parents(location_id)
 
 
 def test_get_demographic_dimensions():
