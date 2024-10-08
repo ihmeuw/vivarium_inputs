50e932dee34a427282f78395238bb1ba9cc778e7
Steve Bachmeier <sbachmei@uw.edu>
2024-10-08 16:16:39 -0700

    add test for getting sequela incidence rate

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -9,7 +9,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import pytest
-from gbd_mapping import causes
+from gbd_mapping import Cause, Sequela, causes, sequelae
 from layered_config_tree import LayeredConfigTree
 from pytest_mock import MockerFixture
 
@@ -23,7 +23,10 @@ from vivarium_inputs.globals import (
 from vivarium_inputs.interface import get_measure
 from vivarium_inputs.utilities import DataTypeNotImplementedError
 
-CAUSE = causes.hiv_aids
+HIV_AIDS = causes.hiv_aids
+HIV_AIDS_DRUG_SUSCEPTIBLE_TB_WO_ANEMIA = (
+    sequelae.hiv_aids_drug_susceptible_tuberculosis_without_anemia
+)
 LOCATION = 163  # India
 YEAR = 2021
 
@@ -64,6 +67,64 @@ def get_mocked_age_groups() -> dict[int, str]:
     }
 
 
+MOCKED_AGE_GROUP_ENDPOINTS = {
+    "age_start": [
+        0.0,
+        0.07671233,
+        0.5,
+        1.0,
+        2.0,
+        5.0,
+        10.0,
+        15.0,
+        20.0,
+        25.0,
+        30.0,
+        0.01917808,
+        35.0,
+        40.0,
+        45.0,
+        50.0,
+        55.0,
+        60.0,
+        65.0,
+        70.0,
+        75.0,
+        80.0,
+        85.0,
+        90.0,
+        95.0,
+    ],
+    "age_end": [
+        0.07671233,
+        0.5,
+        2.0,
+        1.0,
+        5.0,
+        10.0,
+        15.0,
+        20.0,
+        25.0,
+        30.0,
+        0.01917808,
+        35.0,
+        40.0,
+        45.0,
+        50.0,
+        55.0,
+        60.0,
+        65.0,
+        70.0,
+        75.0,
+        80.0,
+        85.0,
+        90.0,
+        95.0,
+        125.0,
+    ],
+}
+
+
 def get_mocked_location_path_to_global() -> pd.DataFrame:
     """Mocked location path to global for testing."""
     return pd.read_csv("tests/fixture_data/location_path_to_global.csv")
@@ -79,9 +140,8 @@ def get_mocked_location_ids() -> pd.DataFrame:
     return pd.read_csv("tests/fixture_data/location_ids.csv")
 
 
-@pytest.fixture
-def mocked_hiv_aids_incidence_rate_draws() -> pd.DataFrame:
-    """Mocked vivarium_gbd_access draw-level data for testing."""
+def get_mocked_common_incidence_rate_draws() -> pd.DataFrame:
+    """Common dataset for mocked incidence rate draw-level data."""
     age_group_ids = list(get_mocked_age_groups())
     sex_ids = [1, 2]
     measure_ids = [3, 5, 6]
@@ -93,7 +153,6 @@ def mocked_hiv_aids_incidence_rate_draws() -> pd.DataFrame:
     )
 
     # Add on other metadata columns
-    df["cause_id"] = CAUSE.gbd_id.real
     df["location_id"] = LOCATION
     df["year_id"] = YEAR
     df["metric_id"] = 3  # for rate
@@ -103,6 +162,18 @@ def mocked_hiv_aids_incidence_rate_draws() -> pd.DataFrame:
     np.random.seed(42)
     for col in DRAW_COLUMNS:
         df[col] = np.random.uniform(0, 0.0004, size=len(df))
+
+    return df
+
+
+@pytest.fixture
+def mocked_incidence_rate_cause_draws() -> pd.DataFrame:
+    """Mocked vivarium_gbd_access data for testing."""
+
+    df = get_mocked_common_incidence_rate_draws().copy()
+
+    # Add on specific columns
+    df["cause_id"] = int(HIV_AIDS.gbd_id)
 
     # Reorder the columns based on what I saw returned from gbd at some point
     df = df[
@@ -115,8 +186,35 @@ def mocked_hiv_aids_incidence_rate_draws() -> pd.DataFrame:
 
 
 @pytest.fixture
-def mocked_hiv_aids_incidence_rate_means() -> pd.DataFrame:
-    """Mocked vivarium_gbd_access mean data for testing."""
+def mocked_incidence_rate_sequela_draws() -> pd.DataFrame:
+    """Mocked vivarium_gbd_access data for testing."""
+
+    df = get_mocked_common_incidence_rate_draws().copy()
+
+    # Add on specific columns
+    df["sequela_id"] = int(HIV_AIDS_DRUG_SUSCEPTIBLE_TB_WO_ANEMIA.gbd_id)
+
+    # Reorder the columns based on what I saw returned from gbd at some point
+    df = df[
+        ["age_group_id"]
+        + DRAW_COLUMNS
+        + [
+            "location_id",
+            "measure_id",
+            "sequela_id",
+            "sex_id",
+            "year_id",
+            "metric_id",
+            "version_id",
+        ]
+    ]
+
+    return df
+
+
+def get_mocked_common_incidence_rate_means() -> pd.DataFrame:
+    """Common dataset for mocked incidence rate mean-level data."""
+
     age_groups = get_mocked_age_groups()
     age_group_ids = list(age_groups)
     sex_ids = [1, 2]
@@ -129,18 +227,15 @@ def mocked_hiv_aids_incidence_rate_means() -> pd.DataFrame:
     )
 
     # Add on other metadata columns
-    df["cause_id"] = CAUSE.gbd_id.real
     df["location_id"] = LOCATION
     df["year_id"] = YEAR
     df["metric_id"] = 3  # for rate
-    df["acause"] = "hiv"
     df["age_group_name"] = df["age_group_id"].map(age_groups)
-    df["cause_name"] = "HIV/AIDS"
     df["expected"] = False
     df["location_name"] = "India"
     df["location_type"] = "admin0"
-    df["measure"] = df["measure_id"].map({5: "incidence", 6: "prevalence"})
-    df["measure_name"] = df["measure_id"].map({5: "Incidence", 6: "Prevalence"})
+    df["measure"] = df["measure_id"].map({5: "prevalence", 6: "incidence"})
+    df["measure_name"] = df["measure_id"].map({5: "Prevalence", 6: "Incidence"})
     df["metric_name"] = "Rate"
     df["sex"] = df["sex_id"].map({1: "Male", 2: "Female"})
 
@@ -150,6 +245,57 @@ def mocked_hiv_aids_incidence_rate_means() -> pd.DataFrame:
         df[col] = np.random.uniform(0, 0.0004, size=len(df))
     df["upper"] = df["val"] * 1.1
     df["lower"] = df["val"] * 0.9
+
+    return df
+
+
+@pytest.fixture
+def mocked_incidence_rate_sequela_means() -> pd.DataFrame:
+    """Mocked vivarium_gbd_access data for testing."""
+
+    df = get_mocked_common_incidence_rate_means().copy()
+
+    # Add on other metadata columns
+    df["sequela_id"] = int(HIV_AIDS_DRUG_SUSCEPTIBLE_TB_WO_ANEMIA.gbd_id)
+    df["sequela_name"] = "HIV/AIDS -  Drug-susceptible Tuberculosis without anemia"
+
+    # Reorder the columns based on what I saw returned from gbd at some point
+    df = df[
+        [
+            "age_group_id",
+            "location_id",
+            "measure_id",
+            "metric_id",
+            "sequela_id",
+            "sex_id",
+            "year_id",
+            "age_group_name",
+            "expected",
+            "location_name",
+            "location_type",
+            "measure",
+            "measure_name",
+            "metric_name",
+            "sequela_name",
+            "sex",
+        ]
+        + MEAN_COLUMNS
+        + ["upper", "lower"]
+    ]
+
+    return df
+
+
+@pytest.fixture
+def mocked_incidence_rate_cause_means() -> pd.DataFrame:
+    """Mocked vivarium_gbd_access data for testing."""
+
+    df = get_mocked_common_incidence_rate_means().copy()
+
+    # Add on other metadata columns
+    df["cause_id"] = int(HIV_AIDS.gbd_id)
+    df["acause"] = "hiv"
+    df["cause_name"] = "HIV/AIDS"
 
     # Reorder the columns based on what I saw returned from gbd at some point
     df = df[
@@ -191,14 +337,20 @@ def no_cache(mocker: MockerFixture) -> None:
 
 
 @pytest.mark.parametrize(
+    "entity", [HIV_AIDS, HIV_AIDS_DRUG_SUSCEPTIBLE_TB_WO_ANEMIA], ids=("cause", "sequela")
+)
+@pytest.mark.parametrize(
     "data_type", ["means", "draws", ["means", "draws"]], ids=("means", "draws", "means_draws")
 )
 @pytest.mark.parametrize("mock_gbd", [True, False], ids=("mock_gbd", "no_mock_gbd"))
 def test_get_incidence_rate(
+    entity: Cause | Sequela,
     data_type: str | list[str],
     mock_gbd: bool,
-    mocked_hiv_aids_incidence_rate_draws: pd.DataFrame,
-    mocked_hiv_aids_incidence_rate_means: pd.DataFrame,
+    mocked_incidence_rate_cause_draws: pd.DataFrame,
+    mocked_incidence_rate_cause_means: pd.DataFrame,
+    mocked_incidence_rate_sequela_draws: pd.DataFrame,
+    mocked_incidence_rate_sequela_means: pd.DataFrame,
     runslow: bool,
     mocker: MockerFixture,
 ) -> None:
@@ -216,7 +368,7 @@ def test_get_incidence_rate(
         pytest.skip("need --runslow option to run")
 
     kwargs = {
-        "entity": CAUSE,
+        "entity": entity,
         "measure": "incidence_rate",
         "location": LOCATION,
         "years": YEAR,
@@ -229,10 +381,17 @@ def test_get_incidence_rate(
     else:
         if mock_gbd:
             # Test against mocked data instead of actual data retrieval
-            mocked_return = {
-                "means": mocked_hiv_aids_incidence_rate_means,
-                "draws": mocked_hiv_aids_incidence_rate_draws,
-            }[data_type]
+            # TODO: Clean up parameterizing by sequela/cause vs means/draws
+            if isinstance(entity, Cause):
+                mocked_return = {
+                    "means": mocked_incidence_rate_cause_means,
+                    "draws": mocked_incidence_rate_cause_draws,
+                }[data_type]
+            else:  # Sequela
+                mocked_return = {
+                    "means": mocked_incidence_rate_sequela_means,
+                    "draws": mocked_incidence_rate_sequela_draws,
+                }[data_type]
             mock_extract_incidence_rate = mocker.patch(
                 "vivarium_inputs.extract.extract_incidence_rate",
                 return_value=mocked_return[
@@ -267,19 +426,29 @@ def test_get_incidence_rate(
             )
 
             data = get_measure(**kwargs)
-
             mock_extract_incidence_rate.assert_called_once()
             mock_extract_prevalence.assert_called_once()
-            assert not data.empty
-            assert all(col in data.columns for col in SUPPORTED_DATA_TYPES[data_type])
-            assert all(data.notna())
-            assert all(data >= 0)
-
         if not mock_gbd and runslow:
             # Test actual data retrieval
             if NO_GBD_ACCESS:
                 pytest.skip("Need GBD access to run this test")
             data = get_measure(**kwargs)
-            assert not data.empty
-            assert all(col in data.columns for col in SUPPORTED_DATA_TYPES[data_type])
-            assert all(data.notna())
+
+        # Check data
+        assert not data.empty
+        assert all(col in data.columns for col in SUPPORTED_DATA_TYPES[data_type])
+        value_cols = SUPPORTED_DATA_TYPES[data_type]
+        assert all(col in data.columns for col in value_cols)
+        assert all(data.notna())
+        assert all(data >= 0)
+        # Check metadata index values (note that there may be other metadata returned)
+        expected_metadata = {
+            "location": {"India"},
+            "sex": {"Male", "Female"},
+            "age_start": set(MOCKED_AGE_GROUP_ENDPOINTS["age_start"]),
+            "age_end": set(MOCKED_AGE_GROUP_ENDPOINTS["age_end"]),
+            "year_start": {2021},
+            "year_end": {2022},
+        }
+        for idx, expected in expected_metadata.items():
+            assert set(data.index.get_level_values(idx)) == expected
