c78052252a148d53d83f8a82b48cff011c006ce8
Steve Bachmeier <sbachmei@uw.edu>
2024-10-16 15:21:01 -0700

    clean up conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import logging
+from typing import Any
 
 import pytest
 from _pytest.logging import caplog as _caplog
-from gbd_mapping import causes, etiologies, risk_factors
 from loguru import logger
 
 
@@ -40,52 +40,14 @@ def caplog(_caplog):
     logger.remove(handler_id)
 
 
-@pytest.fixture
-def cause_list():
-    return [
-        causes.diarrheal_diseases,
-        causes.ischemic_heart_disease,
-        causes.ischemic_stroke,
-        causes.hemorrhagic_stroke,
-        causes.tetanus,
-        causes.diabetes_mellitus,
-        causes.all_causes,
-    ]
-
-
-@pytest.fixture
-def etiology_list():
-    return [etiologies.cholera, etiologies.amoebiasis]
-
-
-@pytest.fixture
-def sequela_list():
-    return list(
-        causes.diarrheal_diseases.sequelae
-        + causes.ischemic_heart_disease.sequelae
-        + causes.ischemic_stroke.sequelae
-        + causes.hemorrhagic_stroke.sequelae
-        + causes.hemorrhagic_stroke.sequelae
-        + causes.tetanus.sequelae
-        + causes.diabetes_mellitus.sequelae
-    )
-
-
-@pytest.fixture
-def etiology_list():
-    return list(
-        causes.diarrheal_diseases.etiologies + causes.lower_respiratory_infections.etiologies
-    )
-
-
-@pytest.fixture
-def risk_list():
-    return [r for r in risk_factors]
-
-
-@pytest.fixture
-def locations():
-    return ["Bangladesh", "Ethiopia", "Kenya", "China", "North Korea", "Nigeria"]
+def pytest_report_teststatus(report: Any, config: Any) -> tuple[Any, str, str] | None:
+    if report.when == "call" and report.skipped:
+        if "todo" in report.longreprtext.lower():
+            return (report.outcome, "T", "TODO")
+        if "fixme" in report.longreprtext.lower():
+            # NOTE: We mark the non-verbose status as "T" (the same as a TODO) because
+            # "F" is reserved for failures.
+            return (report.outcome, "T", "FIXME")
 
 
 def _no_gbd_access():
